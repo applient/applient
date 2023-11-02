fdf97ba8ba10aafd653e941305ba27160992f9b7
Alon Valadji <alon@ronin.co.il>
2023-11-02 02:18:42 +0200

    :sparkles: theme switcher

--- libs/ui/components/.storybook/preview.ts
+++ libs/ui/components/.storybook/preview.ts
@@ -1,1 +1,20 @@
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
+
 import '../../tailwind-preset/src/global.css';
+
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+    attributeName: 'class',
+  }),
+];
+
+export const parameters = {
+  backgrounds: {
+    disable: true,
+  },
+};
